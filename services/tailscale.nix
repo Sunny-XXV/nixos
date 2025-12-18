8ded9d3fe5b03396c0d132a3dcc0c8f708cf7537
sunya <ydsunyan123@gmail.com>
2025-12-18 13:54:24 +0800

    tailscale use current machine as relay server

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -6,6 +6,12 @@
   services.tailscale = {
     enable = true;
     openFirewall = true;
+    permitCertUid = "nginx";
     useRoutingFeatures = "server";
   };
+
+  services.tailscale.extraSetFlags = [
+    "--relay-server-port=41232"
+  ];
+  networking.firewall.allowedUDPPorts = [41232];
 }
